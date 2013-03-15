05b7e2da367e6184e9dd6001068131c9c9d88e5a
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-03-15 16:06:32 -0300

    Use Opscode Omnibus installer for Chef
    All official Digital Ocean images should be supported by it.
    
    At least Debian might not have curl in the base image, so use wget for all.

--- scripts/chef/debian.sh
+++ scripts/chef/debian.sh
@@ -1,20 +1,5 @@
-# cargo culted from OpsCode's guide at http://wiki.opscode.com/display/chef/Installing+Chef+Client+on+Ubuntu+or+Debian
-# skip if the keys are present
+# skip if chef-solo is present
 if (which chef-solo); then exit 0; fi
 
-# install the basics
-apt-get install -y wget lsb-release;
-
-# add the opscode repo to the sources
-echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list;
-
-# setup the gpg key
-mkdir -p /etc/apt/trusted.gpg.d;
-gpg --keyserver keys.gnupg.net --recv-keys 83EF826A;
-gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null;
-
-# update the repo to make sure that the package is available from the repo
-apt-get update;
-
-# avoid server url popup
-echo "chef chef/chef_server_url string https://api.opscode.com/organizations/vagrant" | debconf-set-selections && apt-get install chef -y;
+# download and run the omnibus installer
+wget -O - http://www.opscode.com/chef/install.sh | bash
