78c260c8454f816313a444b347eab2950d22a25d
Shawn Dahlen <shawn@dahlen.me>
2013-03-23 06:51:19 -0400

    implemented ssh key support for digital ocean vagrant provider

--- lib/vagrant-digitalocean/actions/create.rb
+++ lib/vagrant-digitalocean/actions/create.rb
@@ -20,20 +20,18 @@ module VagrantPlugins
             return @app.call(env)
           end
 
-          # TODO check the content of the key to see if it's changed
-          # TODO use the directory / vm name to qualify the key name
+          # TODO check the content of the key to see if it has changed
+          ssh_key_name = env[:machine].provider_config.ssh_key_name
           begin
             ssh_key_id = @client
               .request("/ssh_keys/")
-              .find_id(:ssh_keys, :name => "Vagrant Insecure")
+              .find_id(:ssh_keys, :name => ssh_key_name)
           rescue Errors::ResultMatchError
             env[:ui].info @translator.t("create_key")
 
-            key = DigitalOcean.vagrant_key
-
             result = @client.request("/ssh_keys/new", {
-              :name => "Vagrant Insecure",
-              :ssh_pub_key => key
+              :name => ssh_key_name,
+              :ssh_pub_key => pub_ssh_key(env)
             })
 
             ssh_key_id = result["ssh_key"]["id"]
@@ -98,6 +96,19 @@ module VagrantPlugins
           destroy_env[:force_confirm_destroy] = true
           env[:action_runner].run(Action.new.destroy, destroy_env)
         end
+
+        private
+
+        def pub_ssh_key(env)
+          path = env[:machine].provider_config.pub_ssh_key_path
+          file = File.open(File.expand_path(path))
+          key = file.read
+          file.close
+          key
+        rescue
+          env[:ui].info @translator.t("key_not_found")
+          DigitalOcean.vagrant_key
+        end 
       end
     end
   end
--- lib/vagrant-digitalocean/config.rb
+++ lib/vagrant-digitalocean/config.rb
@@ -1,24 +1,29 @@
 module VagrantPlugins
   module DigitalOcean
     class Config < Vagrant.plugin("2", :config)
-      attr_accessor :client_id, :api_key, :image, :region, :size, :ca_path
+      attr_accessor :client_id, :api_key, :image, :region, :size, :ca_path,
+                    :ssh_key_name, :pub_ssh_key_path
 
       def initialize
-        @client_id       = UNSET_VALUE
-        @api_key         = UNSET_VALUE
-        @image           = UNSET_VALUE
-        @region          = UNSET_VALUE
-        @size            = UNSET_VALUE
-        @ca_path         = UNSET_VALUE
+        @client_id        = UNSET_VALUE
+        @api_key          = UNSET_VALUE
+        @image            = UNSET_VALUE
+        @region           = UNSET_VALUE
+        @size             = UNSET_VALUE
+        @ca_path          = UNSET_VALUE
+        @ssh_key_name     = UNSET_VALUE
+        @pub_ssh_key_path = UNSET_VALUE
       end
 
       def finalize!
-        @client_id = ENV["DO_CLIENT_ID"] if @client_id == UNSET_VALUE
-        @api_key   = ENV["DO_API_KEY"] if @api_key == UNSET_VALUE
-        @image     = "Ubuntu 12.04 x32 Server" if @image == UNSET_VALUE
-        @region    = "New York 1" if @region == UNSET_VALUE
-        @size      = "512MB" if @size == UNSET_VALUE
-        @ca_path   = nil if @ca_path == UNSET_VALUE
+        @client_id         = ENV["DO_CLIENT_ID"] if @client_id == UNSET_VALUE
+        @api_key           = ENV["DO_API_KEY"] if @api_key == UNSET_VALUE
+        @image             = "Ubuntu 12.04 x32 Server" if @image == UNSET_VALUE
+        @region            = "New York 1" if @region == UNSET_VALUE
+        @size              = "512MB" if @size == UNSET_VALUE
+        @ca_path           = nil if @ca_path == UNSET_VALUE
+        @ssh_key_name      = "Vagrant" if @ssh_key_name == UNSET_VALUE
+        @pub_ssh_key_path  = nil if @pub_ssh_key_path == UNSET_VALUE
       end
 
       def validate(machine)
