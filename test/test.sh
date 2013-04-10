bd4b0dfee3b87583e8dec9c8712aaa639d8ad0f9
Shawn Dahlen <shawn@dahlen.me>
2013-04-10 16:43:34 -0400

    Update box definition and testing procedure.
    This commit removes files associated to building an example box. The
    basic box required for use still exists and is not modified during
    testing.

--- test/test.sh
+++ test/test.sh
@@ -1,26 +1,8 @@
-set -e
-
-# make sure bsdtar is installed
-if ! (which bsdtar > /dev/null); then
-  echo "!! Install bsdtar"
-  exit 1
+if ! vagrant box list | grep digital_ocean 1>/dev/null; then
+    vagrant box add digital_ocean box/digital_ocean.box
 fi
 
-# move into the dummy box dir
-cd box
-
-# create the dummy box
-tar cvzf digital_ocean.box ./metadata.json
-
-# remove an old version of the dummy box
-if `vagrant box list | grep -q digital_ocean`; then
-  vagrant box remove digital_ocean digital_ocean
-fi
-
-# add the new version of the dummy box
-vagrant box add digital_ocean digital_ocean.box
-
-cd ../test
+cd test
 
 vagrant up --provider=digital_ocean
 vagrant up
@@ -28,3 +10,5 @@ vagrant provision
 vagrant rebuild
 vagrant halt
 vagrant destroy
+
+cd ..
