4be53c8bae8359d32015128dcafd4d5a0196481d
Alessandro Grassi <alessandro@aggro.it>
2014-07-02 15:36:46 +0200

    Shutdown droplets by default. Support poweroff with vagrant halt -f.

---
+++ lib/vagrant-digitalocean/actions/shut_down.rb
@@ -0,0 +1,33 @@
+require 'vagrant-digitalocean/helpers/client'
+
+module VagrantPlugins
+  module DigitalOcean
+    module Actions
+      class ShutDown
+        include Helpers::Client
+
+        def initialize(app, env)
+          @app = app
+          @machine = env[:machine]
+          @client = client
+          @logger = Log4r::Logger.new('vagrant::digitalocean::shut_down')
+        end
+
+        def call(env)
+          # submit shutdown droplet request
+          result = @client.request("/droplets/#{@machine.id}/shutdown")
+
+          # wait for request to complete
+          env[:ui].info I18n.t('vagrant_digital_ocean.info.shutting_down')
+          @client.wait_for_event(env, result['event_id'])
+
+          # refresh droplet state with provider
+          Provider.droplet(@machine, :refresh => true)
+
+          @app.call(env)
+        end
+      end
+    end
+  end
+end
+
