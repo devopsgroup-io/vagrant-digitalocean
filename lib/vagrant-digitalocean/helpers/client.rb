fbfac5b2a8e86770d6e7f1dfc7d5302c500203b7
Jeff Hertzler <jeffhertzler@gmail.com>
2014-07-31 01:41:51 -0400

    Fix pagination parsing

--- lib/vagrant-digitalocean/helpers/client.rb
+++ lib/vagrant-digitalocean/helpers/client.rb
@@ -60,8 +60,9 @@ module VagrantPlugins
               next_page = body["links"]["pages"]["next"] rescue nil
               unless next_page.nil?
                 uri = URI.parse(next_page)
-                next_result = self.request("#{path}?#{uri.query}")
-                req_target = path.split("/")[-1]
+                new_path = path.split("?")[0]
+                next_result = self.request("#{new_path}?#{uri.query}")
+                req_target = new_path.split("/")[-1]
                 body["#{req_target}"].concat(next_result["#{req_target}"])
               end
             rescue JSON::ParserError => e
